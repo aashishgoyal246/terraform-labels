74d30ce8228f99168f240dfda5999f44c49f2280
Aashish Goyal <aashishgoyal.246@gmail.com>
2020-07-02 17:17:49 +0530

    Updated variables.tf

--- variables.tf
+++ variables.tf
@@ -18,24 +18,6 @@ variable "environment" {
   description = "Environment (e.g. `prod`, `dev`, `staging`)."
 }
 
-variable "label_order" {
-  type        = list
-  default     = []
-  description = "Label order, e.g. `name`,`application`."
-}
-
-variable "attributes" {
-  type        = list
-  default     = []
-  description = "Additional attributes (e.g. `1`)."
-}
-
-variable "tags" {
-  type        = map(string)
-  default     = {}
-  description = "Additional tags (e.g. map(`BusinessUnit`,`XYZ`)."
-}
-
 variable "createdby" {
   type        = string
   default     = "terraform"
@@ -48,6 +30,12 @@ variable "managedby" {
   description = "Managed by, eg 'Aashish Goyal'."
 }
 
+variable "attributes" {
+  type        = list
+  default     = []
+  description = "Additional attributes (e.g. `1`)."
+}
+
 variable "enabled" {
   type        = bool
   default     = false
@@ -58,4 +46,16 @@ variable "delimiter" {
   type        = string
   default     = "-"
   description = "Delimiter to be used between `organization`, `name`, `environment` and `attributes`."
+}
+
+variable "label_order" {
+  type        = list
+  default     = []
+  description = "Label order, e.g. `name`,`application`."
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "Additional tags (e.g. map(`BusinessUnit`,`XYZ`)."
 }
